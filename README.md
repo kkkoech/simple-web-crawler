c4d0bdf369be9974656e47137a988f9735a626c8
kkkoech <kkoech@wesleyan.edu>
2019-06-27 23:08:28 -0400

    fix syntax errors and getting started

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # simple-web-crawler
 A simple web crawler in the Python programming language.
+
+# getting started
+- Run `bash ./install_packages.sh `.
--- crawler.py
+++ crawler.py
@@ -8,21 +8,19 @@ def fetch_results(search_term, number_results, language_code):
     assert isinstance(search_term, str), 'Search term must be a string'
     assert isinstance(number_results, int), 'Number of results must be an integer'
     escaped_search_term = search_term.replace(' ', '+')
-
     google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results, language_code)
     response = requests.get(google_url, headers=USER_AGENT)
     response.raise_for_status()
 
     return search_term, response.text
 
-
+#test
 keyword, html = fetch_results('Vintage hiking shoes men', 100, 'en')
-print(html)
 
 soup = BeautifulSoup(html, 'html.parser')
 
 #prettifying content
-print(soup.prettify()
+print(soup.prettify())
 
 link_s = []
 for bar in soup.findAll("a", attrs={'href': re.compile("^htt")}):
